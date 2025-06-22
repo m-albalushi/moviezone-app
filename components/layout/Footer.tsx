596c37890a6d79f734560297a878a46689e757bf
rocks tony <ton21sey@gmail.com>
2025-06-22 20:29:08 +0400

    Restore full site after maintenance

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,11 +1,10 @@
-
 import React from 'react';
 
 const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-gray-400 py-8 text-center mt-auto">
       <div className="container mx-auto px-6">
-        <p>&copy; {new Date().getFullYear()} MovieZone. All rights reserved .</p>
+        <p>&copy; {new Date().getFullYear()} MovieZone. All rights reserved.</p>
       </div>
     </footer>
   );
