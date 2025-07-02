13e2e5b97e496e06341af8c5fcc312d477976dce
rocks tony <ton21sey@gmail.com>
2025-07-02 23:23:47 +0400

    Fix: update supabaseClient and App.tsx

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from './types';
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://qyazzryjvnmxncurswmd.supabase.co'; 
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5YXp6cnlqdm5teG5jdXJzd21kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwMTczNTIsImV4cCI6MjA2NTU5MzM1Mn0.T-nuFhSFzmg-Zd1WdWyZM6JZ7CwL9rPx9eGk7l4VZJk';
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
